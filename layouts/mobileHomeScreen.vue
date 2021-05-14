1c2d64babd1ba241724db7a1ff10b46bc34e37a8
unknown <marcia.cole@longbridgetech.com>
2021-05-14 10:23:46 +0100

    fix: default page update

--- layouts/mobileHomeScreen.vue
+++ layouts/mobileHomeScreen.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="wrapper">
-    <div class="wrapper__background"></div>
-    <div class="wrapper__content">
+    <div class="wrapper__background">
       <header>
         <img class="home-icon" src="../assets/images/logo.svg" alt="logo" />
       </header>
+    </div>
+    <div class="wrapper__content">
       <div class="mobile_footer">
+        <div style="padding: 10px 18px">
+          <nuxt />
+        </div>
         <img
           src="~assets/images/footer-pattern-individual.png"
           alt="footer-pattern"
         />
-        <div style="padding: 10px 18px">
-          <nuxt />
-        </div>
       </div>
     </div>
   </div>
@@ -24,10 +25,11 @@
   height: 100vh;
   header {
     margin-top: 0.5rem;
-    text-align: center;
+    padding: 0 10px;
+    text-align: right;
   }
   &__background {
-    height: 40%;
+    height: 45%;
     background-image: url('../assets/images/background-image-individual.jpg');
     background-position: center;
     background-repeat: no-repeat;
@@ -35,10 +37,15 @@
   }
   &__content {
     position: relative;
+    height: 55%;
     .mobile_footer {
-      //   position: absolute;
-      //   top: 0;
+      // position: absolute;
+      // bottom: 0;
+      margin-top: 1rem;
+      height: 100%;
       img {
+        position: absolute;
+        bottom: 0;
         width: 100%;
         object-fit: contain;
       }
