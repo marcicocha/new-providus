0b62cb4f74c49cf60bb4d394ea1e8f09370cda09
unknown <marcia.cole@longbridgetech.com>
2021-08-17 00:17:43 +0100

    fix: update on dev

--- static/daon/face/auto.js
+++ static/daon/face/auto.js
@@ -92,7 +92,7 @@ function captureSingle() {
       const c2 = document.getElementById('face-captured')
       const ctx2 = c2.getContext('2d')
       const image = document.querySelector('#image')
-      ctx2.drawImage(image, 50, 40, 360, 700)
+      ctx2.drawImage(image, 0, 0, 360, 700)
     }
 
     document.blob = blob
