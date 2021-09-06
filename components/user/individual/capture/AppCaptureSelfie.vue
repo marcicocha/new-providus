54891ddefe7fa25d8f7bd1f10fcbcd5cd2854cb2
unknown <marcia.cole@longbridgetech.com>
2021-09-06 09:48:38 +0100

    fix: update on camera

--- components/user/individual/capture/AppCaptureSelfie.vue
+++ components/user/individual/capture/AppCaptureSelfie.vue
@@ -179,7 +179,7 @@ export default {
       image.onload = function () {
         const c2 = document.getElementById('face-captured')
         const ctx2 = c2.getContext('2d')
-        ctx2.drawImage(image, 60, 70, 360, 495, 0, 0, c2.width, c2.height)
+        ctx2.drawImage(image, 60, 80, 360, 495, 170, 40, 300, 400)
         img.src = c2.toDataURL('image/jpeg')
       }
       // setTimeout(() => {
